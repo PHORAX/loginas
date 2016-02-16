18a038514d853eb650a0cbc70a9f8dd58edacb93
Tizian Schmidlin <st@cabag.ch>
2016-02-16 09:44:07 +0100

    cabag_loginas version 1.1.6

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,3 +4,10 @@
 CREATE TABLE sys_domain (
 	tx_cabagfileexplorer_redirect_to tinytext,
 );
+
+#
+# Table structure for table 'fe_users'
+#
+CREATE TABLE fe_users (
+	tx_cabagloginas_loginas tinytext,
+);
